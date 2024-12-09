55e1664c56e46ce0ccdc2e026c847e4a93b05b06
lispcat <187922791+lispcat@users.noreply.github.com>
2024-12-09 01:20:29 -0500

    electric pair

--- components/modules/languages/nekomimi-languages.el
+++ components/modules/languages/nekomimi-languages.el
@@ -7,7 +7,10 @@
 ;;   :config
 ;;   (add-hook 'prog-mode-hook #'smartparens-mode))
 
-(electric-pair-mode 1)
+
+(use-package-local elec-pair
+  :config
+  (electric-pair-mode 1))
 
 ;; TODO: consider adding lang to every filename?
 
