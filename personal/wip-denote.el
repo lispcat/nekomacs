802e240f1a68dc5de2986fe80699b202830db08b
lispcat <187922791+lispcat@users.noreply.github.com>
2024-12-14 16:16:38 -0500

    denote and emms

--- personal/wip-denote.el
+++ personal/wip-denote.el
@@ -2,9 +2,16 @@
 ;; https://protesilaos.com/emacs/denote#h:5d16932d-4f7b-493d-8e6a-e5c396b15fd6
 (use-package denote
   :config
-  (setq denote-directory (expand-file-name "~/Denote"))
-  (setq denote-known-keywords '("emacs" "class" "ideas"))
+  (setq denote-directory (expand-file-name "~/Notes/denote"))
+  (setq denote-known-keywords '("emacs" "class" "ideas" "art" "hobbies" "random"))
   ;; (setq denote-prompts '(title keywords subdirectory)) ; cannot interact with rest
+
+  ;; silos
+  (require 'denote-silo-extras)
+  (setq denote-silo-extras-directories
+	(list denote-directory
+	      (expand-file-name "~/School/classes/denote")))
+  
 
   (mimi/leader-define-key
     "n" '(:ignore t :which-key t)
--- personal/wip-fun.el
+++ personal/wip-fun.el
@@ -3,22 +3,50 @@
 ;; no cover
 ;; (use-package listen)
 
+;; emms extract metadata?
+;; https://www.reddit.com/r/emacs/comments/981khz/emacs_music_player_with_emms/
+
 ;; TODO: 
 (use-package emms
   :config
   (require 'emms-setup)
-  ;; (require 'emms-player-mpd)
-  ;; (require 'emms-browser)
+  (require 'emms-player-mpd)
+  (require 'emms-player-mpv)
   (emms-all)
-  (setq emms-player-list '(emms-player-mpv)
-	;; emms-info-functions '(emms-info-exiftool) ; lots o lag
-	emms-source-file-default-directory "~/Music/Library/"
-	;; emms-player-mpv-parameters '("--no-audio-display=no"); broken
-	emms-browser-covers #'emms-browser-cache-thumbnail-async)
+  
+  ;; variables
+  
+  (setq emms-source-file-default-directory "~/Music/library/")
+  ;; emms-player-mpv-parameters '("--no-audio-display=no"); broken
+  (setq emms-browser-covers #'emms-browser-cache-thumbnail-async)
+  ;; sort by natural order
+  (setq emms-playlist-sort-function #'emms-playlist-sort-by-natural-order)
+  ;; sort album by natural order
+  (setq  emms-browser-album-sort-function #'emms-playlist-sort-by-natural-order)
+
+  ;; backends
+  
+  (setq emms-player-list '(emms-player-mpd emms-player-mpv))
+
+  ;; get info from mpd
+  (add-to-list 'emms-info-functions 'emms-info-mpd)
+  ;; ? show current song when next song starts?
+  (add-hook 'emms-player-started-hook #'emms-show)
+  ;; connect to mpd
+  (setq emms-player-mpd-server-name "localhost")
+  (setq emms-player-mpd-server-port "6600")
+  (setq emms-player-mpd-music-directory "\~/Music/library")
+  (emms-player-mpd-connect)
+
+  ;; enable playerctl pausing
+  
+  (require 'emms-mpris)
+  (emms-mpris-enable)
+
   ;; (setq emms-player-list '(emms-player-mpd))
   ;; (add-to-list 'emms-info-functions 'emms-info-mpd)
   ;; (add-to-list 'emms-player-list 'emms-player-mpd)
-
+ 
   ;; TODO: add this function to emms-info-functions (hard to implement?)
   ;; (instead make my own function that runs ffprobe and gets info? might be better)
   (defun my/emms-show-album-cover-in-emacs ()
@@ -32,9 +60,10 @@
 	    (delete-file cover-path))
 	  (let ((exit-code
 		 (shell-command
-		  (format "ffmpeg -i %s -an -vcodec copy %s -y"
-			  (shell-quote-argument song-path)
-			  (shell-quote-argument cover-path)))))
+		  (message "extracting: %s"
+			   (format "ffmpeg -i %s -an -vcodec copy %s -y"
+				   (shell-quote-argument song-path)
+				   (shell-quote-argument cover-path))))))
 	    (cond ((/= exit-code 0)
 		   (message "Error: ffmpeg cover extraction failed with code %s"
 			    exit-code))
